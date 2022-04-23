1b97ae9f0c72861bb547adee7eaf5771b906e69e
unknown <eceashok1997@gmail.com>
2022-04-24 00:36:45 +0530

    Added POST endpoint for account

--- main.go
+++ main.go
@@ -3,13 +3,22 @@ package main
 import (
 	"fmt"
 
+	"github.com/ProjectTravelPartner/accmgtms/api"
 	"github.com/ProjectTravelPartner/commonclient"
+	"github.com/ProjectTravelPartner/dbclient"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	fmt.Println("Hey, there!")
 	port := commonclient.CliPort(3000)
+	dbclient.Init()
+	defer dbclient.Close()
 	router := gin.New()
 	router.Run(fmt.Sprintf(":%v", port))
+	group := router.Group("accmgts")
+
+	group.GET("/account/:id", api.AccountGet)
+	group.POST("/account", api.AccountCreate)
+	group.DELETE("/account/:id", api.AccountDelete)
 }
